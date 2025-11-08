fb45649b466841501bd887a37642947faa74832d
rice <rice9547@gmail.com>
2025-11-08 14:23:21 +0800

    feat: add TypeScript interfaces for API requests and responses

---
+++ src/types/api.ts
@@ -0,0 +1,80 @@
+export interface Location {
+  id: string;
+  lnt: number;
+  lat: number;
+  point: number;
+}
+
+export interface Coordinate {
+  lnt: number;
+  lat: number;
+}
+
+export interface Mission {
+  id: string;
+  name: string;
+  locations: Location[];
+}
+
+export interface LoginRequest {
+  id: string;
+}
+
+export interface LoginResponse {
+  token: string;
+}
+
+export interface MissionsListRequest {
+  lnt: number;
+  lat: number;
+}
+
+export interface MissionsListResponse {
+  missions: Mission[];
+}
+
+export interface MissionDetailRequest {
+  id: string;
+}
+
+export interface MissionDetailResponse {
+  mission: Mission;
+}
+
+export interface CreateQuestRequest {
+  mission_id: string;
+}
+
+export interface CreateQuestResponse {
+  message: string;
+  quest_id: string;
+}
+
+export interface SubmitQuestRequest {
+  paths: Coordinate[];
+}
+
+export interface SubmitQuestResponse {
+  points: number;
+  time_spent: string;
+  distance: number;
+}
+
+export interface QuestOverviewResponse {
+  mission_id: string;
+  path: Coordinate[];
+  points: number;
+  time_spent: string;
+  distance: number;
+}
+
+export interface PathData {
+  id: string;
+  path: Coordinate[];
+  time_spent: string;
+  distance: number;
+}
+
+export interface MissionPathsResponse {
+  paths: PathData[];
+}
